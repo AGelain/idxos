07675cfa407059e40e8a25204bc7af115cdac728
Julio Lobo <juliolobo@lobohost.com.br>
2023-12-28 20:26:02 -0300

    Alterando referência e favicon

--- application/views/tema/rodape.php
+++ application/views/tema/rodape.php
@@ -1,7 +1,9 @@
 <div class="row-fluid">
-    <div id="footer" class="span12"> <a class="pecolor" href="https://github.com/RamonSilva20/mapos" target="_blank">
-            <?= date('Y'); ?> &copy; Ramon Silva - Map-OS - Versão:
-      <?= $this->config->item('app_version'); ?></a></div>
+    <div id="footer" class="span12">
+        <a class="pecolor" href="https://autocrm.com.br" target="_blank">
+            <?= date('Y') ?> &copy; <?php echo $this->config->item('app_name') ?> - Versão: <?= $this->config->item('app_version'); ?>
+        </a>
+    </div>
 </div>
 <!--end-Footer-part-->
 <script src="<?= base_url(); ?>assets/js/bootstrap.min.js"></script>
